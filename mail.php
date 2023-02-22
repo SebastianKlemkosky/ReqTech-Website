70c8c1008e6dae72537c795fc4626d27e80bd2d1
Sebs <sebastianklemkosky@gmail.com>
2023-02-22 10:12:33 -0600

    Updated PHP script
    Updated PHP script and fixed navbar so that is should be more responsive

--- mail.php
+++ mail.php
@@ -1,17 +1,26 @@
 <?php
+// Set the recipient email address
+$to = "sebastianklemkosky@gmail.com";
 
+// Get the form fields
 $name = $_POST['name'];
 $email = $_POST['email'];
+$phone = $_POST['phone'];
 $message = $_POST['message'];
-$subject = "This is a Test of PHP Contact Form";
 
-$mailheader = "From:".$name."<".$email.">\r\n";
+// Format the email message
+$subject = "New Contact Form Submission";
+$body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message";
 
-$recipient = "sebs.klemkoskyalt@gmail.com";
+// Set the email headers
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-mail($recipient, $subject, $message, $mailheader) or die("Error!");
-
-
+// Send the email
+if (mail($to, $subject, $body, $headers)) {
+    // If the email was sent successfully, redirect to a thank-you page
+    
 echo "<!DOCTYPE html>
 <html>
   <head>
@@ -41,5 +50,41 @@ echo "<!DOCTYPE html>
    
   </body>
 </html>";
+    exit;
+} else {
+    // If there was an error sending the email, display an error message
+    echo "<!DOCTYPE html>
+<html>
+  <head>
+    <title>ReqTech</title>
+    <link rel='stylesheet' type='text/css' href='style.css'>
+    <link
+    rel='stylesheet'
+    href='https://use.fontawesome.com/releases/v5.8.2/css/all.css'
+    integrity='sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay'
+    crossorigin='anonymous'
+    />  
+  </head>
+  <body>
+    <header>
+      
+    </header>
 
+
+    <!-- Start Message -->
+    <section id='message' class='contact'>
+      <h2>There was an error sending your message</h2>
+      <p>Please try again later</p>
+      <p class='back'>Go back to the <a href='index.html'>home page</a></p>
+    </section>
+    <!-- End Message -->
+
+   
+  </body>
+</html>";
+}
 ?>
+
+
+
+
