c639e06e7a1293b5ac443885f4eed8b6aee83fa5
Sebs <sebastianklemkosky@gmail.com>
2023-03-14 05:38:55 -0500

    Completed Facebook Review Code
    -Code will pull reviews using facebook API

--- send.php
+++ send.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     
     // Set the recipient email address
-    $to = 'Rudy@Reqtech.net, reqtech.website@gmail.com';
+    $to = 'Rudy@Reqtech.net, reqtech.website@gmail.com, Sebastian@reqtech.net';
 
 
     // Set the email subject
