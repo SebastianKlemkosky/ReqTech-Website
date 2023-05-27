5059e448438c90dcddb4d5826b1286f94c85cc35
Sebs <sebastianklemkosky@gmail.com>
2023-05-27 14:20:31 -0500

    Changed  gear into pressure washing

--- pull_facebook_reviews.js
+++ pull_facebook_reviews.js
@@ -1,4 +1,4 @@
-const accessToken = '';
+const accessToken = 'EAAKYP9uj8a4BAHYmlVKBWYIZAAAqiaZC9CtOGiMkkf0aXVor1irNTgCNkYM5IHDzGS8VMKO7D49GDp5cte5ZCgPhqqjIAZBvHAT7ZAPN8GxoUOZAZBLRj8KstZCmOseBdBbfWz9YOTrZAZByZByZBPC9DJ9rgDvM9ZBFJh0WF9GHc9Cl3DZAimupGmHZCvmZCGA7vHZCKAIIbZCSfiON9YwlE73s2F8Hit';
 const pageId = '107672455547761';
 
 fetch(`https://graph.facebook.com/v16.0/${pageId}/ratings?fields=review_text,reviewer{name}&access_token=${accessToken}`)
@@ -9,13 +9,17 @@ fetch(`https://graph.facebook.com/v16.0/${pageId}/ratings?fields=review_text,rev
     
     for (let i = 0; i < reviews.length; i++) {
       const review = reviews[i].review_text;
-      const reviewerName = reviews[i].reviewer ? reviews[i].reviewer.name : 'Facebook Review';
+      const reviewerName = reviews[i].reviewer ? reviews[i].reviewer.name : '- Facebook Review';
       const reviewElement = document.createElement('div');
       reviewElement.classList.add('review');
-      reviewElement.innerHTML = `
-        <p class="review-text">${review}</p>
-        <p class="reviewer-name">-${reviewerName}</p>
-      `;
+      reviewElement.innerHTML =   `
+      <div class="review">
+        <p>${review}</p>
+        <div class="author">
+          <span class="name">${reviewerName}</span>
+        </div>
+      </div>
+    `;
       reviewContainer.appendChild(reviewElement);
     }
   })
